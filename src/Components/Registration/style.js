e997d66c6f2c929dabc8a0867ef74f9615e15609
nishantbodke123 <nishant.bodke@bhugolgis.com>
2023-08-02 19:46:49 +0530

    CHV Questioning part done

--- src/Components/Registration/style.js
+++ src/Components/Registration/style.js
@@ -1,4 +1,5 @@
-import { Col, Form, Row, Tabs } from "antd";
+import { Button, Col, Form, Input, Row, Tabs } from "antd";
+import TextArea from "antd/es/input/TextArea";
 import { styled } from "styled-components";
 
 export const Container = styled.div`
@@ -9,41 +10,176 @@ export const FormContainer = styled(Form)`
   margin: 20px 40px;
 `;
 
+export const InputForm = styled(Input)`
+  width: 300px;
+`;
+
+export const TextAreaForm = styled(TextArea)`
+  width: 300px;
+`;
+
 export const Column = styled(Col)`
   width: 33.33%;
+
+  @media (max-width: 820px) {
+    width: 70%;
+  }
+  @media (max-width: 450px) {
+    width: 100%;
+  }
 `;
 export const FormItem = styled(Form.Item)`
   width: 350px;
+
+  @media (max-width: 820px) {
+    width: 450px;
+  }
+  @media (max-width: 450px) {
+    width: 300px;
+  }
 `;
 
 export const FormHeader = styled.p`
   font-weight: 650;
   margin: 30px 25px;
+
+  // @media (max-width: 820px) {
+  //   width: 450px;
+  // }
+  // @media (max-width: 450px) {
+  //   width: 300px;
+  // }
 `;
 export const QuestionRow = styled(Row)`
   background-color: #dde6ed;
   margin: 20px 20px;
+
+  // @media (max-width: 820px) {
+  //   width: 450px;
+  // }
+  // @media (max-width: 450px) {
+  //   width: 300px;
+  // }
+`;
+export const QuestionSubRow = styled(Row)`
+  margin: 8px 100px;
+  @media (max-width: 820px) {
+  }
+  @media (max-width: 450px) {
+    margin: 8px 50px;
+  }
 `;
 export const QuestionCol = styled(Col)`
   width: 35%;
   margin: 20px 20px;
+
+  @media (max-width: 820px) {
+    width: 100%;
+    margin: 20px 20px;
+  }
+  // @media (max-width: 450px) {
+  //   width: 300px;
+  // }
 `;
+export const QuestionSubCol = styled(Col)`
+  width: 30%;
+
+  @media (max-width: 820px) {
+    width: 60%;
+  }
+  @media (max-width: 450px) {
+    width: 80%;
+  }
+`;
+
 export const AnswerCol = styled(Col)`
   margin: 20px 200px;
+  @media (max-width: 820px) {
+    width: 100%;
+    margin: 20px 20px;
+  }
+  // @media (max-width: 450px) {
+  //   width: 300px;
+  // }
+`;
+export const AnswerSubCol = styled(Col)`
+  width: 20%;
+
+  @media (max-width: 820px) {
+    width: 40%;
+  }
+  @media (max-width: 450px) {
+    width: 20%;
+  }
 `;
 export const DocsTab = styled(Tabs)`
   .ant-tabs-tab-btn {
     color: black;
     font-size: 15px;
     font-weight: 500;
-    margin: 10px 70px;
+    margin: 10px 50px;
   }
   .ant-tabs-tab.ant-tabs-tab-active .ant-tabs-tab-btn {
     color: #ff8551;
     font-weight: 700;
-    font-size: 18px;
+    font-size: 15px;
   }
   .ant-tabs-ink-bar {
     background-color: #ff8551;
+  }
+
+  @media (max-width: 820px) {
+    .ant-tabs-tab-btn {
+      color: black;
+      font-size: 13px;
+      font-weight: 600;
+      margin: 10px 0px;
+    }
+    .ant-tabs-tab.ant-tabs-tab-active .ant-tabs-tab-btn {
+      color: #ff8551;
+      font-weight: 500;
+      font-size: 15px;
+    }
+    .ant-tabs-ink-bar {
+      background-color: #ff8551;
+    }
+  }
+  @media (max-width: 450px) {
+    .ant-tabs-tab-btn {
+      color: black;
+      font-size: 10px;
+      font-weight: 600;
+    }
+    .ant-tabs-tab.ant-tabs-tab-active .ant-tabs-tab-btn {
+      color: #ff8551;
+      font-weight: 700;
+      font-size: 12px;
+    }
+    .ant-tabs-ink-bar {
+      background-color: #ff8551;
+    }
+  }
+`;
+
+export const SubmitButtonDiv = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin: 20px 100px;
+
+  @media (max-width: 820px) {
+    margin: 20px 320px;
+  }
+  @media (max-width: 450px) {
+    margin: 20px 120px;
+  }
+`;
+
+export const SubmitButton = styled(Button)`
+  background-color: #ff8551;
+  color: black;
+  font-weight: 600;
+  &:hover {
+    background-color: #f29727;
+    color: none;
   }
 `;
