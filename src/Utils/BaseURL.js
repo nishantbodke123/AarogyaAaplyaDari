727005441bdcf99b894c1ddd8926fa09598770b1
nishatbodke123 <nishantbodke@gmail.com>
2023-10-09 14:04:36 +0530

    dashboard responsive

--- src/Utils/BaseURL.js
+++ src/Utils/BaseURL.js
@@ -1,5 +1,5 @@
 //server internal
 // export const BASE_URL = "http://172.16.0.40:9061";
 //server external
-// export const BASE_URL="http://202.189.224.222:9061"
- export const BASE_URL="http://10.202.100.236:9061"
+export const BASE_URL="http://202.189.224.222:9061"
+// export const BASE_URL = "http://10.202.100.236:9061";
