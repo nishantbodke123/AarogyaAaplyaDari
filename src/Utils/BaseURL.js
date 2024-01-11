56168666b5a24b4c0b8517091103966a43fd5ae7
nishatbodke123 <nishantbodke@gmail.com>
2024-01-11 11:50:20 +0530

    BASEURL Changed

--- src/Utils/BaseURL.js
+++ src/Utils/BaseURL.js
@@ -1,8 +1,8 @@
 //server internal
 // export const BASE_URL = "http://172.16.0.40:9061";
 //server external
-// export const BASE_URL = "http://202.189.224.222:9061";
+export const BASE_URL = "http://202.189.224.222:9061";
 // export const BASE_URL = "http://10.202.100.236:9061";
 
 //jafar's system
-export const BASE_URL = "http://10.202.100.224:9061";
+// export const BASE_URL = "http://10.202.100.224:9061";
